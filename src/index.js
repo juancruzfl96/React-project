10939591f5ea446ea575ac515e1541619d50ec26
juancruzfl96 <juancruzfl96@github.com>
2021-06-09 02:44:01 -0300

    this is my first commit. I created the components, and worked on te header.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './components/header/header';
+import './index.css';
+
+ReactDOM.render(<Header />, document.getElementById('root'));
