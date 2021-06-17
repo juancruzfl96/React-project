5bdad0bb7af4a09d2aabbd072c36e338a2d383e2
juancruzfl96 <juancruzfl96@github.com>
2021-06-17 20:48:54 -0300

    fix styles in production mode

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,17 +1,14 @@
 import './style.css';
-import {Button} from 'reactstrap';
 
 const Card = ({ character, onClick }) => {
 
 	return (
-		<div  className="card">
-			<Button  color='white' onClick={() => onClick(character)} className="buttonCharacter">
+		<div  onClick={() => onClick(character)} className="card">		
 				<img
 					src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
 					className='cardImg' height='100%' width='100%'
 				/>
-				<h3 className="name">{character.name}</h3>
-			</Button>
+				<span className="name">{character.name}</span>
 		</div>
 	);
 };
